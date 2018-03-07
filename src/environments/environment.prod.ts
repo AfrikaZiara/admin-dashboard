3c9fe5c9ca1f91de470ea9f0bf8e7036d170b8f0
Elkana Rop <elkanarop@gmail.com>
2018-03-07 19:06:56 +0000

    Firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,12 @@
  */
 export const environment = {
   production: true,
+  firebase:{
+    apiKey: "AIzaSyCmuZwTXAmunGqeweSiEeFuasbHwvnbslM",
+    authDomain: "afrika-ziara.firebaseapp.com",
+    databaseURL: "https://afrika-ziara.firebaseio.com",
+    projectId: "afrika-ziara",
+    storageBucket: "afrika-ziara.appspot.com",
+    messagingSenderId: "259941993438"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,4 +10,12 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: "AIzaSyCmuZwTXAmunGqeweSiEeFuasbHwvnbslM",
+    authDomain: "afrika-ziara.firebaseapp.com",
+    databaseURL: "https://afrika-ziara.firebaseio.com",
+    projectId: "afrika-ziara",
+    storageBucket: "afrika-ziara.appspot.com",
+    messagingSenderId: "259941993438"
+  }
 };
