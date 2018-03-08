ffdbcef4716419e302d67f2badcdbcf05bb3dd8f
Elkana Rop <elkanarop@gmail.com>
2018-03-08 02:28:25 +0000

    fix navigation

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -25,38 +25,6 @@ export const MENU_ITEMS: NbMenuItem[] = [
         title: 'Manage Tour Operators',
         link: '/pages/tables/smart-table',
       },
-      {
-        title: 'Buttons',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Popovers',
-        link: '/pages/ui-features/popovers',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
     ],
   },
   {
@@ -70,14 +38,6 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Manage Packages',
         link: '/pages/tables/smart-table',
-      },
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
       },
     ],
   },
@@ -191,6 +151,23 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Popovers',
         link: '/pages/ui-features/popovers',
+      },
+      {
+        title: 'Animated Searches',
+        link: '/pages/ui-features/search-fields',
+      },
+      {
+        title: 'Tabs',
+        link: '/pages/ui-features/tabs/tab1',
+      },
+      {
+        title: 'Layout Forms',
+        link: '/pages/forms/layouts',
+
+      },
+      {
+        title: 'Form Inputs',
+        link: '/pages/forms/inputs',
       },
     ],
   },
